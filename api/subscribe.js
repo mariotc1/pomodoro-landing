cd9607fda397d58bd2a4b285a9dd6284b3b80ca8
Mario <mariotomecore@gmail.com>
2025-09-06 16:12:20 +0200

    Corrección subscribe.js y merge tag para MailerLite

--- api/subscribe.js
+++ api/subscribe.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         email,
-        fields: { name: name || "" },
+        fields: { Nombre: name || "" },
         groups: ["164707869286990870"],
         resubscribe: true
       })
